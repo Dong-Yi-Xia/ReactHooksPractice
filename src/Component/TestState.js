22ab200c4548177e9096dcb8c94bca0f26117d34
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-11-29 21:01:48 -0500

    setState

--- src/Component/TestState.js
+++ src/Component/TestState.js
@@ -3,25 +3,41 @@ import React, {useState} from 'react'
 
 
 let Num = () => {
+    console.log("running a function")
     return 5
   }
 
 function TestState (){
 
-    let [count, setCount] = useState( Num() )
+    //useState callback function will render 1 time on inital. Just passing in Num() will re-render everytime onClick.
+    let [count, setCount] = useState( () => Num() )
+
+    //implied arrow function. else {return "blue"}
+    let [color, setColor] = useState( () => "blue" )
+
+    const randomColor = () => {
+      let colors = ["blue", "red", "green", "pink", "yellow"]
+      let randNum = Math.floor(Math.random() * colors.length )
+      return colors[randNum]
+    } 
 
     let subtracting = () =>{
+      setCount(preCount => preCount - 1)
+      setColor(randomColor())
       console.log("minus here")
     }
   
     function adding(){
+      setCount(preCount => preCount + 1)
+      setColor(randomColor())
       console.log("i am add")
     }
     return(
         <div>
             <button onClick={subtracting}>-</button>
-            <span>{count}</span>
             <button onClick={adding}>+</button>
+            <span>{count}</span>
+            <span>{color}</span>
         </div>
     )
 }
